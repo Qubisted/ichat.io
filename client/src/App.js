aa844d9bde384a5652d6f14a0c860973e7254b4c
Udhay <udhay1co.de@gmai.com>
2021-10-11 19:54:11 +0530

    seting up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
 import React from 'react';
+
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    'Hello world'
+    <h1>Home page</h1>
   )
 }
 
