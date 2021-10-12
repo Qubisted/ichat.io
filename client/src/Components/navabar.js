14219737a887f2d4e26bac6c852017c147273dd7
Udhay <udhay1co.de@gmai.com>
2021-10-12 11:50:31 +0530

    navabar home page

---
+++ client/src/Components/navabar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, NavbarBrand, Container,Nav } from 'react-bootstrap';
+import './navbar.css'
+import logo from './head.svg'
+function nav() {
+    return(
+        <Navbar>
+  <Container>
+
+
+    {/* <Navbar.Brand id="nav-link">IChat</Navbar.Brand> */}
+
+    <Navbar.Brand href="#home">
+      <img
+        src={logo}
+        width="150"
+        height="150"
+        className="d-inline-block align-top  "
+        alt="React Bootstrap logo"
+      />
+      </Navbar.Brand>
+
+      {/* Sign in button */}
+
+    <a  className="button w-button ">Sign in</a>
+
+    {/* Waitlist button */}
+
+  </Container>
+</Navbar>
+  
+ 
+
+    )
+}
+
+export default nav;
--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,14 +1,14 @@
-
 import React from 'react';
-  
-function About () {
-    return <div>
-        <h2>GeeksforGeeks is a computer science portal for geeks!</h2>
-  
-        Read more about us at : 
-        <a href="https://www.geeksforgeeks.org/about/">
-            https://www.geeksforgeeks.org/about/
-        </a>
-    </div>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../Components/navabar.js'
+
+function login() {
+    return(
+
+        <Navbar />
+        
+    )
+
 }
-export default About;
+
+export default login;
