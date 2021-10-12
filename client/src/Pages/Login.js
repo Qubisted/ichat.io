7afe0cb8bf2ea540888503fa0ae059b283476373
Udhay <udhay1co.de@gmai.com>
2021-10-12 12:11:07 +0530

    images

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '../Components/navabar.js'
-import image_center from './hero-img.png';
+import image_center from './hero-1.jpg';
+import './login.css'
 function login() {
     return(
         <div>
@@ -11,7 +12,7 @@ function login() {
         src={image_center}
         width="800"
         height="800"
-        className="d-inline-block align-top  "
+        className="d-inline-block align-top img-1 "
         alt="React Bootstrap logo"
       />
 
