3ba3616a011c293e526cd1371d9f004843295a05
Udhay <udhay1co.de@gmai.com>
2021-10-12 16:28:41 +0530

    login page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '../Components/navabar.js'
-import image_center from './hero-img.png';
+import image_center from './man-holding-phone-edit.svg';
 import './login.css'
 function login() {
     return(
         <div>
         <Navbar />
 
-        <img
+    
+
+
+      <h1 className="heading">Signup Now!</h1>
+
+      <p className="paragraph-2">Apply to join the Ichat.io Network.<br />
+       We are even looking for Contributers For this Project project across the planet<br />
+      </p>
+
+      
+
+      <img
         src={image_center}
         width="800"
         height="800"
@@ -16,8 +27,9 @@ function login() {
         alt="React Bootstrap logo"
       />
 
+      <a className="button-2 sign-in-styles">Login</a>
 
-      <h1 className="heading">IChat.io || Signup Now!</h1>
+     
 
         </div>
 
