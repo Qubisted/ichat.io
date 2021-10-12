db199a26758deb92cdb6ad5136b94e2b269c3842
Udhay <udhay1co.de@gmai.com>
2021-10-12 20:44:19 +0530

    nav-routes for rediert

--- client/src/Components/home-navabar.js
+++ client/src/Components/home-navabar.js
@@ -3,9 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, NavbarBrand, Container,Nav } from 'react-bootstrap';
 import logo from '../Components/head.svg';
 import './home-navbar.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 function homeNav(){
     return(
-        <Navbar>
+        <Router>
+            {/* Route paths for navbars */}
+         <Navbar>
         <Container>
             <NavbarBrand>
                 {/* Image designed with fighma */}
@@ -17,10 +21,19 @@ function homeNav(){
         alt="React Bootstrap logo"
       />
 
+      <Link to={'/login'}>
+          <button>test</button>
+      </Link>
+    
+
                 
             </NavbarBrand>
         </Container>
     </Navbar>
+
+
+
+    </Router>
     )
 
 
